bf85a4e7eb94ce418754d19780db317dfaa50d01
unknown <k_sheth@u.pacific.edu>
2018-04-30 22:45:44 -0700

    fixing marine_shared file

--- marine_shared.php
+++ marine_shared.php
@@ -1,19 +1,17 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = new Dotenv\Dotenv(__DIR__);
-if(getenv('APP_ENV') === 'development')
-{
-$dotenv->load();
-}
-
-$dotenv->required(['DATABASE_URL']);
-
 $dbopts = parse_url(getenv('DATABASE_URL'));
 
-$dbname = ltrim($dbopts["path"], '/');
+$dbopts["path"] = ltrim($dbopts["path"], "/");
 
-$db = new PDO("$dbopts[scheme]:host=$dbopts[host];dbname=$dbname;port=$dbopts[port]",
-    $dbopts["user"], $dbopts["pass"], array(PDO::MYSQL_ATTR_INIT_COMMAND =>
-    "SET NAMES utf8"));
+$db = new PDO("pgsql:" . sprintf(
+"host=%s;port=%s;user=%s;password=%s;dbname=%s",
+$dbopts["host"],
+$dbopts["port"],
+$dbopts["user"],
+$dbopts["pass"],
+ltrim($dbopts["path"], "/")
+));
+
 ?>
